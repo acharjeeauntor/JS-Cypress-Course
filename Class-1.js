2d3514b3cf805db495caeeaf9120e15913835147
acharjeeauntor <acharjeeauntor@gmail.com>
2023-11-18 16:16:37 +0600

    Added Class 1 file

---
+++ Class-1.js
@@ -0,0 +1,88 @@
+// Output & Syntax, Comments::::::::::::::
+
+// var age = 20
+// var country = "BD"
+
+// console.log("My Age is", age)
+// console.log(`My Age is ${age}. And i am from ${country}`)
+
+
+
+// Variable,Let,Const::::::::::::::::
+
+// Var - we can re-declear, re-assign
+
+// var age  = 10
+// var age = 10
+//  age = 50
+
+
+
+// Let - we can re-assign
+// let age  = 10
+// // let age = 10
+// age = 50
+// console.log(age)
+
+
+
+// Const - we can declear only, we can not re-assign or re-declear
+
+// const age  = 10
+// // age = 50
+// console.log(age)
+
+
+
+
+// Operators,Arithmetic,Assignment:::::::
+
+// Arithmetic Operators
+
+// var x = 10;
+// var y  = 20;
+
+// console.log(x+y)
+
+
+
+// Comparison Operators
+
+// var x = 20;
+// var y  = '20';
+// var y  = 20;
+
+// is equal to
+// console.log(x==y)
+// console.log(x===y)
+
+
+// console.log(x>=y) // greater than or equal to
+// console.log(x<=y)
+
+// Bitwise Operators
+
+// var x = 20;
+// var y  = 10;
+
+// console.log(10==10 & 10==20)   
+// console.log(10==20 | 20==20)
+
+
+
+// Assignment Operators
+
+// var x = 20;
+// var y  = 10;
+
+// console.log(x+=1)
+
+
+// Logical Operators (&&,||,!) 
+
+// var x = 20;
+// var y  = 10;
+
+// console.log(10==10 && 10==20)   
+// console.log(10==20 || 20==20)
+// console.log(!3489734) //Logical Not
