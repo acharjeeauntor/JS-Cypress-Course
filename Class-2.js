aadd47f1f947fcde76b26e327b8ce559c541864d
acharjeeauntor <acharjeeauntor@gmail.com>
2023-11-21 07:56:24 +0600

    added class 2 content

---
+++ Class-2.js
@@ -0,0 +1,136 @@
+// data type::::
+// let name ="jo"
+// console.log(typeof(name))
+
+// let age = 16.67877
+// console.log(typeof(age))
+
+// let x = true
+//  console.log(typeof(x))
+
+//  const person = {firstName:"Jo",from:"Aus"}
+//  console.log(typeof(person))
+
+
+// Function::::::
+
+// function myAction(){
+//     // Block of Code
+//     // fill the email
+//     // fill the pass
+//     // press login button
+// }
+
+
+// function sum(a,b){
+//     return {add:a+b,sub:a-b}
+// }
+
+// console.log(sum(5,7))
+// var x = sum(6,8)
+// console.log(x.sub)
+
+
+
+// Object::::::
+
+// object - properties [name,color,model number] - Methods [stop(),drive()]
+
+
+
+// const car = {
+//     name:"BMW",
+//     model: "5000",
+//     color:"White",
+//     fullDetails:function(age){
+//     return `The car name is ${this.name}. the model is ${this.model}. Color is ${this.color}. The age of the car is ${age}`
+//        }
+// }
+
+// // var color = car.color
+// // var name = car.name
+
+// // console.log(`The name of the car is ${name}. the color is ${color}`)
+
+
+
+// console.log(car.fullDetails(15))
+
+
+// Condition:::::
+
+// var x,y,z
+// if(x==y){
+
+// }else if(x==z){
+
+// }else{
+
+// }
+
+
+// var age = 16
+// if(age%2==0){
+// console.log("Even")
+// }else if(age=="new"){
+//     console.log("new")
+// }
+// else{
+//     console.log("odd")
+// }
+
+
+// var age = 16
+
+// if(age%2==0){
+// console.log("Even")
+// }else if(age=="new"){
+//     console.log("new")
+// }else if(age=="new1"){
+//     console.log("new1")
+// }
+// else if(age=="new2"){
+//     console.log("new2")
+// }
+// else if(age=="new3"){
+//     console.log("new3")
+// }
+// else{
+//     console.log("odd")
+// }
+
+
+// switch(age%2){
+// case 0:
+//     console.log("Even")
+//     break
+// case "new":
+//     console.log("new")
+//     break
+// case "new1":
+//     console.log("new1")
+//     break
+// case "new2":
+//     console.log("new2")
+//     break
+// case "new3":
+//     console.log("new3")
+//     break
+// default:
+//     console.log("no one")
+// }
+
+
+
+
+
+
+
+
+
+
+
+// Comparisons::::::
+
+
+
