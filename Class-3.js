7b5f000f62e81082292972ccafb2b6f72aad46ce
acharjeeauntor <acharjeeauntor@gmail.com>
2023-12-01 14:02:50 +0600

    Added Class 3&4

---
+++ Class-3.js
@@ -0,0 +1,124 @@
+// String Methods
+/*
+String length -
+String slice() -
+String substring() -
+String substr()
+String replace() -
+String replaceAll()
+String toUpperCase() -
+String toLowerCase() -
+String concat() -
+String trim() -
+String trimStart() -
+String trimEnd() -
+String padStart()
+String padEnd()
+String charAt()
+String charCodeAt()
+String split() -
+*/
+
+// var name = "dyfgdhjkfkjd"
+// console.log(name.length)
+
+// let text = "BD, Aus, USA"
+// console.log(text.slice(4,7))
+// console.log(text)
+
+// let text2 = "BD, Aus, USA"
+// console.log(text2.substring(4,7))
+// console.log(text2)
+
+// let text = "BD, Aus, USA"
+// console.log(text.replace("USA","AUS"))
+
+// text = "BD, Aus, USA"
+// console.log(text.toUpperCase())
+// console.log(text.toLowerCase())
+
+
+// var text1 = 'Hello'
+// var text2 = 'world'
+
+// var text3 = text1.concat(" ",text2)
+// console.log(text3)
+
+// var text1 = 'Hello'
+// var text2 = 'world'
+
+// var text3 = `${text1} ${text2}`
+// console.log(text3)
+
+
+// var text1 = '       Hello            '
+// console.log(text1)
+
+// var text2 = text1.trim()
+// console.log(text2)
+
+// var text2 = text1.trimStart()
+// console.log(text2)
+
+// var text2 = text1.trimEnd()
+// console.log(text2)
+
+//  let text = "BD, Aus, USA"
+//  console.log(typeof(text))
+
+// var arrayOfString = text.split(',')
+//  console.log(arrayOfString)
+//  console.log(typeof(arrayOfString))
+//  console.log(arrayOfString[2])
+
+// Array
+
+/*
+Array length - 
+Array toString() - 
+Array pop() -
+Array push() -
+Array shift()
+Array unshift()
+Array join() - 
+Array delete() -
+Array concat() -
+Array flat()
+Array splice() -
+Array slice()
+*/
+
+
+// var text = ["BD","AUS","USA"]
+// console.log(text.length)
+// console.log(text.toString())
+
+// console.log(text.pop())
+
+
+// var text = ["BD","AUS","USA"]
+// text.push("ARG")
+// console.log(text)
+
+// var text = ["BD","AUS","USA"]
+// var text1 = ["ABC","XYZ"]
+// var joinedArray = text.concat(text1)
+// console.log(joinedArray)
+
+// var text = ["BD","AUS","USA"]
+// var newArray = []
+// newArray.push("BD")
+// newArray.push("AUS")
+// console.log(newArray)
+
+
+// var text = ["BD","AUS","USA"]
+// //var text1 = ["ABC","XYZ"]
+
+// var newArr = text.splice(1,2)
+// console.log(newArr)
+
+// var text = ["BD","AUS","USA"]
+// console.log(text.reverse())
+
+
---
+++ Class-4.js
@@ -0,0 +1,36 @@
+// Arrow Function
+
+// function myFn(a,b){
+//     return a+b
+// }
+
+// var x = myFn(5,4)
+// console.log(x)
+
+
+// let myFunction = (a,b) => a+b;
+// console.log(myFunction(5,4))
+
+// let myFunction = (a,b) => {
+// var x = a*10
+// return x+b;
+// }
+// console.log(myFunction(5,4))
+
+
+// hello = () => "Hello World"
+// console.log(hello())
+
+// myfun = val => val*10
+// console.log(myfun(5))
+
+/////// Class:
+
+class Classname{
+    constructor(){
+
+    }
+}
+
+
+var myObj = new Classname("name",22)
